d65ff512b9b7dda63206a6e60140d2ae45d78d1d
Shishatka <Shishatka@gmail.com>
2025-03-17 14:05:15 +0300

    домашка сделана!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Author LeoTolstoy = new Author("Leo", "Tolstoy");
+        Author FyodorDosoevsky = new Author("Fyodor", "Dostoevsky");
+        Book bookA = new Book("War and Peace", LeoTolstoy, 1867);
+        Book bookB = new Book("Crime and Punishment", FyodorDosoevsky, 1860);
+        bookB.setYearOfPublication(1866);
+        System.out.println(bookB.getYearOfPublication());
     }
 }
